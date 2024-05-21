259a60ef4f08a7d09a5236a1d4b42a38c6080706
Mahmoud Nabhan <niypoo@gmail.com>
2024-05-21 10:54:41 +0300

    first

---
+++ lib/appVersion.helper.dart
@@ -0,0 +1,21 @@
+import 'package:app_configuration_service/appInfo.config.dart';
+import 'package:app_version_check/updateAppRequired.page.dart';
+import 'package:firebase_remote_config_service/remoteConfig.service.dart';
+import 'package:get/get.dart';
+
+class AppVersionCheck {
+  static check() {
+    // get version from server
+    final double versionFromServer =
+        RemoteConfigService.to.getDouble('version');
+
+    // get version from app configuration
+    final double appVersion = AppConfigService.to.appVersion;
+
+    // check
+    if (versionFromServer > appVersion) {
+      // show message page
+      return Get.to(const AppUpdateRequiredPage());
+    }
+  }
+}
---
+++ lib/updateAppRequired.page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:fly_ui/extensions/responsive.extension.dart';
+import 'package:fly_ui/views/layouts/landscapeView.widget.dart';
+import 'package:fly_ui/views/layouts/responsiveView.widget.dart';
+import 'package:fly_ui/views/layouts/scaffoldLayout.widget.dart';
+import 'package:get/get.dart';
+import 'package:unicons/unicons.dart';
+
+class AppUpdateRequiredPage extends StatelessWidget {
+  const AppUpdateRequiredPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      child: FlyScaffold.padding(
+        child: const FlyLayoutResponsiveView(
+          portrait: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                AppUpdateIcon(),
+                AppUpdateTitle(),
+                AppUpdateDescription(),
+              ],
+            ),
+          ),
+          landscape: FlyLandscapeView(
+            childA: Center(child: AppUpdateIcon()),
+            childB: SingleChildScrollView(
+              child: Column(
+                children: [
+                  AppUpdateTitle(),
+                  AppUpdateDescription(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AppUpdateDescription extends StatelessWidget {
+  const AppUpdateDescription({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'AppVersionCheck.The app is outdated, Please update to the latest version to continue.'
+          .tr,
+      style: Get.textTheme.titleSmall!.copyWith(
+        fontSize: 9.sp,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class AppUpdateTitle extends StatelessWidget {
+  const AppUpdateTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'AppVersionCheck.Update Required'.tr,
+      style: Get.textTheme.titleLarge!.copyWith(
+        fontSize: 11.sp,
+        color: Get.theme.primaryColor,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+      maxLines: 2,
+    );
+  }
+}
+
+class AppUpdateIcon extends StatelessWidget {
+  const AppUpdateIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10.sp),
+      child: Icon(
+        UniconsLine.arrow_circle_up,
+        size: 20.sp,
+      ),
+    );
+  }
+}
